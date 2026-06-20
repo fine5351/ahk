946e42cdf4896e7fa0af1ae1345a5c7a3854e9f1
fine5351 <fine1101105351@gmail.com>
2026-06-21 07:44:40 +0800

    refactor: update enabled run-ahk-scripts for StarRail and remove Honkai3rd tasks

--- run-ahk-scripts-1k.ps1
+++ run-ahk-scripts-1k.ps1
@@ -3,12 +3,10 @@
 #     Exit
 # }
 
-Start-Process ".\AutoHotkey\v2\AutoHotkey.exe" -ArgumentList ".\Honkai3rd\1k\Guild-Contribution-ShiftF5-1k.ahk" -Verb RunAs
-# Start-Process ".\AutoHotkey\v2\AutoHotkey.exe" -ArgumentList ".\StarRail\1k\Abandoned-ShiftF1-1k.ahk" -Verb RunAs
-# Start-Process ".\AutoHotkey\v2\AutoHotkey.exe" -ArgumentList ".\StarRail\1k\Lock-ShiftF2-1k.ahk" -Verb RunAs
+Start-Process ".\AutoHotkey\v2\AutoHotkey.exe" -ArgumentList ".\StarRail\1k\Abandoned-ShiftF1-1k.ahk" -Verb RunAs
+Start-Process ".\AutoHotkey\v2\AutoHotkey.exe" -ArgumentList ".\StarRail\1k\Lock-ShiftF2-1k.ahk" -Verb RunAs
 # Start-Process ".\AutoHotkey\v2\AutoHotkey.exe" -ArgumentList ".\ZenlessZoneZero\1k\Stage-Abandoned-ShiftF3-1k.ahk" -Verb RunAs
 # Start-Process ".\AutoHotkey\v2\AutoHotkey.exe" -ArgumentList ".\ZenlessZoneZero\1k\Stage-Lock-ShiftF4-1k.ahk" -Verb RunAs
 # Start-Process ".\AutoHotkey\v2\AutoHotkey.exe" -ArgumentList ".\basic\F-AltF.ahk" -Verb RunAs
 # Start-Process ".\AutoHotkey\v2\AutoHotkey.exe" -ArgumentList ".\basic\LeftClick.ahk" -Verb RunAs
 Start-Process ".\AutoHotkey\v2\AutoHotkey.exe" -ArgumentList ".\chrome\mute-Alt1.ahk" -Verb RunAs
-Start-Process ".\AutoHotkey\v2\AutoHotkey.exe" -ArgumentList ".\Honkai3rd\星芽-樂土-Atl1.ahk" -Verb RunAs
